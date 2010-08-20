5892db450b78ab2a0df3295d0c7ce53303e66743
Dave Smith <dave@thesmithfam.org>
2010-08-20 08:02:34 -0600

    Add query capability
     - You can now use the --query command line argument
       to see which zones are on or off at the moment.

--- isprinkle-control/main.c
+++ isprinkle-control/main.c
@@ -3,8 +3,19 @@
 
 #define ALL_OFF_MAGIC_NUMBER 42
 
+int usage(char **argv)
+{
+    printf("Usage: %s <options>\n", argv[0]);
+    printf("\n");
+    printf("  options:\n");
+    printf("    --run-zone <number> Run a single zone (<number> can be 1 through 8).\n");
+    printf("    --all-off           Turns off all zones.\n");
+    return EXIT_FAILURE;
+}
+
 int main(int argc, char **argv)
 {
+    int do_query = 1;
     int zone_number = -1;
     int turn_all_relays_off = 0;
     int ret;
@@ -39,22 +50,17 @@ int main(int argc, char **argv)
         {
             zone_number = ALL_OFF_MAGIC_NUMBER;
         }
+        else if(!strcmp(argv[i], "--query"))
+        {
+            do_query = 1;
+        }
         else
         {
             fprintf(stderr, "\n");
             fprintf(stderr, "Error: Incorrect command line parameter.\n");
             fprintf(stderr, "\n");
+            return usage(argv);
         }
-    }
-
-    if(zone_number == -1)
-    {
-        printf("Usage: %s <options>\n", argv[0]);
-        printf("\n");
-        printf("  options:\n");
-        printf("    --run-zone <number> Run a single zone (<number> can be 1 through 8).\n");
-        printf("    --all-off           Turns off all zones.\n");
-        return EXIT_FAILURE;
     }
 
     if (ftdi_init(&ftdic) < 0)
@@ -81,17 +87,39 @@ int main(int argc, char **argv)
         return EXIT_FAILURE;
     }
 
-    unsigned char relay_control_bitmask = (1 << zone_number);
-    if(ALL_OFF_MAGIC_NUMBER == zone_number)
+    if(zone_number != -1)
     {
-        relay_control_bitmask = 0;
-    }
+        unsigned char relay_control_bitmask = (1 << zone_number);
+        if(ALL_OFF_MAGIC_NUMBER == zone_number)
+        {
+            relay_control_bitmask = 0;
+        }
 
-    ret = ftdi_write_data_async(&ftdic, &relay_control_bitmask, 1);
-    if(ret != 1)
+        ret = ftdi_write_data_async(&ftdic, &relay_control_bitmask, 1);
+        if(ret != 1)
+        {
+            fprintf(stderr, "Could not send data to the relay board: 0x%02x\n", relay_control_bitmask);
+            return EXIT_FAILURE;
+        }
+    }
+    else if(do_query)
     {
-        fprintf(stderr, "Could not send data to the relay board: 0x%02x\n", relay_control_bitmask);
-        return EXIT_FAILURE;
+        unsigned char buf;
+        if ((ret = ftdi_read_data(&ftdic, &buf, 1)) != 1)
+        {
+            fprintf(stderr, "Could not read status from relay board: %d\n", ret);
+            return EXIT_FAILURE;
+        }
+
+        for(i=0; i<8; i++)
+        {
+            int on = (buf & (1 << i));
+            printf("Zone %d: %s\n", i+1, on ? "On" : "Off");
+        }
+    }
+    else
+    {
+        fprintf(stderr, "Woops! Programmer error.\n");
     }
 
     if ((ret = ftdi_usb_close(&ftdic)) < 0)
