82555e15d392615a6da44f9844bdd6aa4412fe9e
Dave Smith <dave@thesmithfam.org>
2010-09-18 10:59:19 -0600

    Helper scripts to ease testing

---
+++ isprinkle-server/test/run-watering-now.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+uuid="$1"
+tmpfile=/tmp/isprinkle-temp-file
+
+if [ -z "$uuid" ]; then
+    echo "Usage: $0 <uuid of watering to run now>"
+    exit 1
+fi
+
+echo "$uuid" > "$tmpfile"
+wget http://localhost:8080/run-watering-now "--post-file=$tmpfile"
