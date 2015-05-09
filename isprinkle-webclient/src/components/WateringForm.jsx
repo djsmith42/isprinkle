b3d538906d10c75cdac635b933a34a5247d51621
Dave Smith <dave@thesmithfam.org>
2015-05-09 10:11:31 -0600

    Better save indicator
    Finished off some more TODO items

--- isprinkle-webclient/src/components/WateringForm.jsx
+++ isprinkle-webclient/src/components/WateringForm.jsx
@@ -2,6 +2,7 @@ var React = require('react');
 var ScheduleTypes = require('../constants').ScheduleTypes;
 var WateringsStore = require('../stores/WateringsStore');
 var ZonesStore = require('../stores/ZonesStore');
+var Spinner = require('react-spinkit');
 
 require('./WateringForm.less');
 
@@ -132,62 +133,70 @@ class WateringForm extends React.Component {
   }
 
   render() {
-    var isSaving = this.state.isSavig;
-    return (
-      <form className="WateringForm" onSubmit={this.preventSubmit}>
-        {this.props.mode === "edit" && <h4>Edit Watering:</h4>}
-        {this.props.mode === "add"  && <h4>New Watering: </h4>}
-        <div className="form-group">
-          <label>Schedule Type:</label>
-          <select disabled={isSaving} ref="scheduleType" defaultValue={this.state.scheduleType} onChange={this.formChanged.bind(this)} className="form-control">
-            <option value={ScheduleTypes.EVERY_N_DAYS}>Every N days</option>
-            <option value={ScheduleTypes.FIXED_DAYS_OF_WEEK}>Specific days of the week</option>
-            <option value={ScheduleTypes.SINGLE_SHOT}>Single shot</option>
-          </select>
+    var isSaving = this.state.isSaving;
+    if (isSaving) {
+      return (
+        <div className="WateringFormSaving">
+          <Spinner spinnerName='three-bounce' noFadeIn />
         </div>
-        {this.state.scheduleType == ScheduleTypes.EVERY_N_DAYS &&
+      )
+    } else {
+      return (
+        <form className="WateringForm" onSubmit={this.preventSubmit}>
+          {this.props.mode === "edit" && <h4>Edit Watering:</h4>}
+          {this.props.mode === "add"  && <h4>New Watering: </h4>}
           <div className="form-group">
-            <label>Period:</label>
-            <input type="number" ref="periodDays" defaultValue={this.state.periodDays} onChange={this.formChanged.bind(this)} className="form-control" />
-          </div>}
-        {this.state.scheduleType == ScheduleTypes.SINGLE_SHOT &&
+            <label>Schedule Type:</label>
+            <select ref="scheduleType" defaultValue={this.state.scheduleType} onChange={this.formChanged.bind(this)} className="form-control">
+              <option value={ScheduleTypes.EVERY_N_DAYS}>Every N days</option>
+              <option value={ScheduleTypes.FIXED_DAYS_OF_WEEK}>Specific days of the week</option>
+              <option value={ScheduleTypes.SINGLE_SHOT}>Single shot</option>
+            </select>
+          </div>
+          {this.state.scheduleType == ScheduleTypes.EVERY_N_DAYS &&
+            <div className="form-group">
+              <label>Period:</label>
+              <input type="number" ref="periodDays" defaultValue={this.state.periodDays} onChange={this.formChanged.bind(this)} className="form-control" />
+            </div>}
+          {this.state.scheduleType == ScheduleTypes.SINGLE_SHOT &&
+            <div className="form-group">
+              <label>Start Date:</label>
+              <input type="text" ref="startDate" defaultValue={this.state.startDate} onChange={this.formChanged.bind(this)} className="form-control" />
+            </div>}
           <div className="form-group">
-            <label>Start Date:</label>
-            <input type="text" ref="startDate" defaultValue={this.state.startDate} onChange={this.formChanged.bind(this)} className="form-control" />
-          </div>}
-        <div className="form-group">
-          <label>Start Time of Day:</label>
-          <input type="text" ref="startTime" defaultValue={this.state.startTime} onChange={this.formChanged.bind(this)} className="form-control" />
-        </div>
-        <div className="form-group">
-          <label>Scheduled Zones:</label>
-          {this.state.zoneDurations.map((zoneDuration, index) => (
-            <div key={index + "-" + zoneDuration.id + "-" + zoneDuration.minutes} className="zone-duration">
-              <button onClick={this.removeZoneDurationClicked.bind(this, index)} className="remove-zone">x</button>
-              <select className="form-control zone-select" onChange={this.zoneDurationZoneIdChanged.bind(this, zoneDuration)} defaultValue={zoneDuration.id}>
-                {this._zones().map((zone) => (
-                  <option key={zone.id} value={zone.id}>{zone.name}</option>
-                ))}
-              </select>
-              <input
-                type="number"
-                className="form-control zone-minutes"
-                onChange={this.zoneDurationMinutesChanged.bind(this, zoneDuration)}
-                defaultValue={zoneDuration.minutes} />
-              <div className="minutes">Minutes</div>
-              <div className="clearfix"></div>
-            </div>
-          ))}
-        </div>
-        <div className="form-group">
-          <button type="button" className="btn btn-default" onClick={this.addZoneClicked.bind(this)}>Add Zone</button>
-        </div>
-        <div>
-          <button disabled={isSaving} type="button" className="btn btn-default" onClick={this.cancelClicked.bind(this)}>Cancel</button>
-          <button disabled={isSaving} type="button" className="btn btn-primary" onClick={this.saveClicked.bind(this)}>Save</button>
-        </div>
-      </form>
-    )
+            <label>Start Time of Day:</label>
+            <input type="text" ref="startTime" defaultValue={this.state.startTime} onChange={this.formChanged.bind(this)} className="form-control" />
+          </div>
+          <div className="form-group">
+            <label>Scheduled Zones:</label>
+            {this.state.zoneDurations.map((zoneDuration, index) => (
+              <div key={index + "-" + zoneDuration.id + "-" + zoneDuration.minutes} className="zone-duration">
+                <button onClick={this.removeZoneDurationClicked.bind(this, index)} className="remove-zone">x</button>
+                <select className="form-control zone-select" onChange={this.zoneDurationZoneIdChanged.bind(this, zoneDuration)} defaultValue={zoneDuration.id}>
+                  {this._zones().map((zone) => (
+                    <option key={zone.id} value={zone.id}>{zone.name}</option>
+                  ))}
+                </select>
+                <input
+                  type="number"
+                  className="form-control zone-minutes"
+                  onChange={this.zoneDurationMinutesChanged.bind(this, zoneDuration)}
+                  defaultValue={zoneDuration.minutes} />
+                <div className="minutes">Minutes</div>
+                <div className="clearfix"></div>
+              </div>
+            ))}
+          </div>
+          <div className="form-group">
+            <button type="button" className="btn btn-default" onClick={this.addZoneClicked.bind(this)}>Add Zone</button>
+          </div>
+          <div>
+            <button type="button" className="btn btn-default" onClick={this.cancelClicked.bind(this)}>Cancel</button>
+            <button type="button" className="btn btn-primary" onClick={this.saveClicked.bind(this)}>Save</button>
+          </div>
+        </form>
+      )
+    }
   }
 }
 
